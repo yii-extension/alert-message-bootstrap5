4717bb05f361c16fc47ff37a4d517c7b1f9921d9
Wilmer Arambula <terabytesoftw@gmail.com>
2020-12-14 14:29:55 -0300

    Initital commit.

---
+++ src/AlertMessage.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yii\Extension\Widget;
+
+use Yiisoft\Html\Html;
+use Yiisoft\Session\Flash\Flash;
+use Yiisoft\Widget\Widget;
+use Yiisoft\Yii\Bootstrap5\Alert;
+
+final class AlertMessage extends Widget
+{
+    private Flash $flash;
+
+    private array $alertTypes = [
+        'danger' => 'alert-danger',
+        'dark' => 'alert-dark',
+        'info' => 'alert-info',
+        'light' => 'alert-light',
+        'primary' => 'alert-primary',
+        'secondary' => 'alert-secondary',
+        'success' => 'alert-success',
+        'warning' => 'alert-warning',
+    ];
+
+    public function __construct(Flash $flash)
+    {
+        $this->flash = $flash;
+    }
+
+    protected function run(): string
+    {
+        $flashes = $this->flash->getAll();
+        $html = '';
+
+        /** @var array $data */
+        foreach ($flashes as $type => $data) {
+            if (isset($this->alertTypes[$type]) && is_string($this->alertTypes[$type])) {
+                /** @var array $message */
+                foreach ($data as $message) {
+                    $body = '';
+                    $closeButtonEnabled = true;
+                    $options = [];
+
+                    if (isset($message['body']) && is_string($message['body'])) {
+                        $body = $message['body'];
+                    }
+
+                    if (isset($message['closeButton']) && is_bool($message['closeButton'])) {
+                        $closeButtonEnabled = $message['closeButton'];
+                    }
+
+                    if (isset($message['options']) && is_array($message['options'])) {
+                        $options = $message['options'];
+                    }
+
+                    /** @psalm-suppress MixedArgumentTypeCoercion */
+                    Html::addCssClass($options, $this->alertTypes[$type]);
+
+                    if ($body !== '') {
+                        $html .= Alert::widget()
+                            ->body($body)
+                            ->closeButtonEnabled($closeButtonEnabled)
+                            ->options($options);
+                    }
+                }
+            }
+        }
+
+        return $html;
+    }
+}
---
+++ tests/AlertMessageTest.php
@@ -0,0 +1,242 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yii\Extension\Widget\Tests;
+
+use Yii\Extension\Widget\AlertMessage;
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+final class AlertMessageTest extends TestCase
+{
+    public function testAlertMessageEmpty(): void
+    {
+        $this->flash->add(
+            'success',
+            [
+                'body' =>  '',
+            ],
+            true,
+        );
+
+        $html = AlertMessage::widget()->render();
+
+        $this->assertEquals('', $html);
+    }
+
+    public function testAlertMessageCheckType(): void
+    {
+        $this->flash->add(
+            'testMe',
+            [
+                'message' => 'Error testMe message',
+            ],
+            true,
+        );
+
+        $this->flash->add(
+            'success',
+            [
+                'body' =>  'Body message',
+            ],
+            true,
+        );
+
+        $html = AlertMessage::widget()->render();
+
+        $expected = <<<HTML
+<div id="w0-alert" class="alert-success alert alert-dismissible" role="alert">Body message
+<button type="button" class="btn-close" aria-label="Close" data-dismiss="alert"></button>
+</div>
+HTML;
+
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testAlertMessageOptions(): void
+    {
+        $this->flash->add(
+            'success',
+            [
+                'body' =>  'Body message',
+                'options' => ['id' => 'testMe'],
+            ],
+            true,
+        );
+
+        $html = AlertMessage::widget()->render();
+
+        $expected = <<<HTML
+<div id="testMe" class="alert-success alert alert-dismissible" role="alert">Body message
+<button type="button" class="btn-close" aria-label="Close" data-dismiss="alert"></button>
+</div>
+HTML;
+
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testAlertMessageMultiple(): void
+    {
+        $this->flash->add(
+            'success',
+            [
+                'body' =>  'Body 1'
+            ],
+            true
+        );
+
+        $this->flash->add(
+            'danger',
+            [
+                'body' =>  'Body 2'
+            ],
+            true
+        );
+
+
+        $html = AlertMessage::widget()->render();
+
+        $expected = <<<HTML
+<div id="w0-alert" class="alert-success alert alert-dismissible" role="alert">Body 1
+<button type="button" class="btn-close" aria-label="Close" data-dismiss="alert"></button>
+</div><div id="w1-alert" class="alert-danger alert alert-dismissible" role="alert">Body 2
+<button type="button" class="btn-close" aria-label="Close" data-dismiss="alert"></button>
+</div>
+HTML;
+
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testAlertMessageDanger(): void
+    {
+        $this->flash->add(
+            'danger',
+            [
+                'body' =>  'Body 1',
+            ],
+            true,
+        );
+
+        $html = AlertMessage::widget()->render();
+
+        $expected = <<<HTML
+<div id="w0-alert" class="alert-danger alert alert-dismissible" role="alert">Body 1
+<button type="button" class="btn-close" aria-label="Close" data-dismiss="alert"></button>
+</div>
+HTML;
+
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testAlertMessageDark(): void
+    {
+        $this->flash->add(
+            'dark',
+            [
+                'header' => 'Header 1',
+                'body' =>  'Body 1',
+            ],
+            true,
+        );
+
+        $html = AlertMessage::widget()->render();
+
+        $expected = <<<HTML
+<div id="w0-alert" class="alert-dark alert alert-dismissible" role="alert">Body 1
+<button type="button" class="btn-close" aria-label="Close" data-dismiss="alert"></button>
+</div>
+HTML;
+
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testAlertMessageInfo(): void
+    {
+        $this->flash->add(
+            'info',
+            [
+                'header' => 'Header 1',
+                'body' =>  'Body 1',
+            ],
+            true,
+        );
+
+        $html = AlertMessage::widget()->render();
+
+        $expected = <<<HTML
+<div id="w0-alert" class="alert-info alert alert-dismissible" role="alert">Body 1
+<button type="button" class="btn-close" aria-label="Close" data-dismiss="alert"></button>
+</div>
+HTML;
+
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testAlertMessagePrimary(): void
+    {
+        $this->flash->add(
+            'primary',
+            [
+                'header' => 'Header 1',
+                'body' =>  'Body 1',
+            ],
+            true,
+        );
+
+        $html = AlertMessage::widget()->render();
+
+        $expected = <<<HTML
+<div id="w0-alert" class="alert-primary alert alert-dismissible" role="alert">Body 1
+<button type="button" class="btn-close" aria-label="Close" data-dismiss="alert"></button>
+</div>
+HTML;
+
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testAlertMessageSuccess(): void
+    {
+        $this->flash->add(
+            'success',
+            [
+                'header' => 'Header 1',
+                'body' =>  'Body 1',
+            ],
+            true,
+        );
+
+        $html = AlertMessage::widget()->render();
+
+        $expected = <<<HTML
+<div id="w0-alert" class="alert-success alert alert-dismissible" role="alert">Body 1
+<button type="button" class="btn-close" aria-label="Close" data-dismiss="alert"></button>
+</div>
+HTML;
+
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testAlertMessageWarning(): void
+    {
+        $this->flash->add(
+            'warning',
+            [
+                'header' => 'Header 1',
+                'body' =>  'Body 1',
+            ],
+            true,
+        );
+
+        $html = AlertMessage::widget()->render();
+
+        $expected = <<<HTML
+<div id="w0-alert" class="alert-warning alert alert-dismissible" role="alert">Body 1
+<button type="button" class="btn-close" aria-label="Close" data-dismiss="alert"></button>
+</div>
+HTML;
+
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+}
